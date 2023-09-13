c2be9756600c3d4b050df7c964a9f3cfca0fd5c0
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-09-13 21:10:41 +0000

    Add a way to show flash messages in flask

--- run.py
+++ run.py
@@ -1,15 +1,27 @@
 # https://flask.palletsprojects.com/en/2.3.x/quickstart/#a-minimal-application
+
 # The first thing we need to do, is import the 'request' library from Flask.
 # Request is going to handle things like finding out what method we used, and it will also
 # contain our form object when we've posted it.
+
+# What we want to do next, is display some feedback to the user.
+# To do that, we are going to import a function from Flask called 'flash'.
+# Sometimes we want to display a non-permanent message to the user, something that only stays
+# on screen until we refresh the page, or go to a different one.
+# These are called 'flashed messages' in Flask.
 import json
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 # capital F of Flask as its a class name
+
+# import only if system finds env.py file
+if os.path.exists("env.py"):
+    import env
 
 # instace of class is app
 #  This is needed so that Flask knows where to look for resources such as templates and static files.
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 # In Python, a decorator starts with the @ symbol, which is also called pie-notation.
 # Effectively, a decorator is a way of wrapping functions.
@@ -47,7 +59,10 @@ def contact():
     if request.method == "POST":
         # print(request.form)
         # print(request.form.get("name")) better option tha below
-        print(request.form["email"])
+        # print(request.form["email"])
+        # The name will be injected in {} below
+        flash("Thanks {}, we have recived your message".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact Us")
 
 
